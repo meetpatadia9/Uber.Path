08da9a550c410f4d7bd3c9fd1c0ff45a62018727
IPS-030 <meetp-itpath@gmail.com>
2023-08-07 16:42:02 +0530

    Activity fragment

--- app/src/main/java/com/ipsmeet/uberpath/fragments/ActivityFragment.kt
+++ app/src/main/java/com/ipsmeet/uberpath/fragments/ActivityFragment.kt
@@ -6,9 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ipsmeet.uberpath.R
+import com.ipsmeet.uberpath.adapter.TransactionAdapter
+import com.ipsmeet.uberpath.databinding.FragmentActivityBinding
+import com.ipsmeet.uberpath.dataclass.TransactionDataClass
 
 class ActivityFragment : Fragment() {
+
+    lateinit var binding: FragmentActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +24,36 @@ class ActivityFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activity, container, false)
+        binding = FragmentActivityBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnBack.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.layout_fragmentLoader, HomeFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        val transactions = arrayListOf<TransactionDataClass>()
+        transactions.apply {
+            add(TransactionDataClass(R.drawable.img_basketball, "Sports Shop", "Payment", "$15.99", "December 28"))
+            add(TransactionDataClass(R.drawable.img_money_receive, "From Test User9", "Received", "$61.18", "November 28"))
+            add(TransactionDataClass(R.drawable.img_money_receive, "Bank of Baroda", "Deposit", "$2,045.00", "October 28"))
+            add(TransactionDataClass(R.drawable.img_send, "To Test User1", "Sent", "$986.00", "September 28"))
+            add(TransactionDataClass(R.drawable.img_line, "Line premium", "Payment", "$24.00", "December 28"))
+            add(TransactionDataClass(R.drawable.img_bitcoin, "Bitcoin", "Investment", "$2,550.99", "November 28"))
+            add(TransactionDataClass(R.drawable.img_paypal, "Paypal", "Freelance", "$2,328.00", "October 28"))
+            add(TransactionDataClass(R.drawable.img_spotify, "Spotify premium", "Payment", "$24.00", "September 28"))
+        }
+
+        binding.recyclerViewTransaction.apply {
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            adapter = TransactionAdapter(requireContext(), transactions)
+        }
     }
 
 }
